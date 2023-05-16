ef005f0456e72a6ae56e41c2af42a34bbdd9aca5
yhwa <hfbc93@gmail.com>
2023-05-16 09:27:32 +0900

    Feat: 비디오 edit watch 화면에 상세  데이터 바인딩

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,52 +3,53 @@ const fakeUser = {
   loggedIn : false,
 }
 
+let videos = [
+  {
+    title:"First Video",
+    rating:4.5,
+    comments:2,
+    createdAt: "12분 전",
+    views:10,
+    id:1,
+  },
+  {
+    title:"Second Video",
+    rating:4.5,
+    comments:2,
+    createdAt: "12분 전",
+    views:10,
+    id:2,
+  },
+  {
+    title:"Third Video",
+    rating:4.5,
+    comments:1,
+    createdAt: "12분 전",
+    views:1,
+    id:3,
+  },
+];
+
 export const trending = (req, res) => {
-
-  const videos = [
-    {
-      title:"First Video",
-      rating:5,
-      comments:2,
-      createdAt: "12분 전",
-      views:10,
-      id:1,
-    },
-    {
-      title:"Second Video",
-      rating:5,
-      comments:2,
-      createdAt: "12분 전",
-      views:10,
-      id:1,
-    },
-    {
-      title:"Third Video",
-      rating:5,
-      comments:2,
-      createdAt: "12분 전",
-      views:10,
-      id:1,
-    },
-  ];
-
   return res.render("home", {pageTitle:"home", fakeUser, videos});
 };
 
-export const search = (req, res) => {
-  return res.render("search", {pageTitle:"search"});
-}; 
-
 export const watch = (req, res) => {
   const { params: {id} } = req;
+  const video = videos[id-1];
   
-  return res.render("watch", {pageTitle:"watch"});
+  return res.render("watch", {pageTitle:`Watching: ${video.title}`, fakeUser, video});
 }; 
 
 export const edit = (req, res) => {
   const { params: {id} } = req;
+  const video = videos[id-1];
 
-  return res.render("edit", {pageTitle:"edit"});
+  return res.render("edit", {pageTitle:`Editing: ${video.title}`, fakeUser, video});
+}; 
+
+export const search = (req, res) => {
+  return res.render("search", {pageTitle:"search"});
 }; 
 
 export const remove = (req, res) => {
