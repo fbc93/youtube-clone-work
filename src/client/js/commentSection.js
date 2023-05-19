029bf6a207288f72a6cc23156e26d1b174795fec
yhwa <hfbc93@gmail.com>
2023-05-19 12:37:17 +0900

    Feat: 댓글 수정 버튼 UI 추가

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,10 +3,28 @@ const videoContainer = document.getElementById("videoContainer");
 
 const addComment = (text) => {
   const videoComments = document.querySelector(".video__comments ul");
+
   const newComment = document.createElement("li");
+  const deleteBtn = document.createElement("span");
+  const updateBtn = document.createElement("span");
+  const comment__text = document.createElement("div");
+  const comment__edit = document.createElement("div");
 
   newComment.className = "video__comment";
-  newComment.innerText = text;
+  deleteBtn.className = "deleteBtn";
+  updateBtn.className = "updateBtn";
+  comment__text.className = "comment__text";
+  comment__edit.className = "comment__edit";
+
+  comment__text.innerText = text;
+  deleteBtn.innerText = "❌";
+  updateBtn.innerText = "✏️";
+
+  comment__edit.appendChild(deleteBtn);
+  comment__edit.appendChild(updateBtn);
+
+  newComment.appendChild(comment__text);
+  newComment.appendChild(comment__edit);
 
   videoComments.prepend(newComment);
 }
